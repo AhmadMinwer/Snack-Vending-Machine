21fd68b13ba0f930d3d4fd04fcbda3fff793ab74
Never <a7mdm.e@gmail.com>
2019-04-26 10:27:49 +0300

    source code of snackVendingMachine

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import javax.xml.soap.SOAPPart;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        //machine initialization
+        Scanner in=new Scanner(System.in);
+        SVM svm = new SVM();
+
+        //machine setup
+        svm.addItem(1,"Cola",5,(float) 0.3);
+        svm.addItem(2,"Pringles",5,(float) 0.5);
+        svm.addItem(3,"Snickers",5,(float) 1);
+        svm.addItem(4,"Kinder ",5,(float) .8);
+        svm.addItem(5,"Kit Kat",5,(float) 1.2);
+        svm.addItem(2,"Raffaello",5,(float) 1.5);
+
+        int state=-1;
+        while(state != 0){
+
+            System.out.println("0. Exit");
+            ArrayList<String> itemsNames = svm.getNames();
+            for(int i=0; i<itemsNames.size() ; i++){
+                System.out.println((i+1)+". "+itemsNames.get(i));
+            }
+            System.out.println("Enter your choice:");
+            state = in.nextInt();
+            if(state ==0)
+                continue;
+
+            System.out.println("you selected : "+ state +" "+ svm.getItemName(state)+ " \nand it cost: "+svm.getItemPrice(state));
+            svm.SelectItem(state);
+        }
+
+    }
+
+}
---
+++ src/SVM.java
@@ -0,0 +1,265 @@
+import javax.xml.soap.SOAPPart;
+import java.util.*;
+
+class Item{
+    String name;
+    float price;
+    int count;
+    int id;
+
+    public Item(int id, String name, int count, float price) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+        this.id = id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public  String getName(){
+        return name;
+    }
+
+    public float selectItem(){
+        if(getCount()>0){
+            this.count--;
+            return this.getPrice();
+        }else{
+            return -1;
+        }
+    }
+}
+
+class Currency{
+    float value;
+    String type;
+
+    public Currency(float value, String type) {
+        this.value = value;
+        this.type = type;
+    }
+}
+
+//class CurrencyList extends ArrayList<Currency>{
+//
+//    // change with limited coins problem
+//    public String getChange(){
+//        if(true) {
+//            return "Change List";
+//        }else{
+//            return "";
+//        }
+//    }
+//}
+
+class SVM {
+    ArrayList<Item> Items;
+    ArrayList<Currency> currencies;
+    float totalRevenue;
+
+    public SVM() {
+        this.Items = new ArrayList<Item>();
+        this.currencies = new ArrayList<Currency>();
+
+        this.currencies.add(new Currency((float) 0.1,"10C"));
+        this.currencies.add(new Currency((float)0.2,"20C"));
+        this.currencies.add(new Currency((float)0.5,"50C"));
+        this.currencies.add(new Currency((float)1.0,"1$"));
+        this.currencies.add(new Currency((float)20.0,"20$"));
+        this.currencies.add(new Currency((float)50.0,"50$"));
+
+        this.totalRevenue = 0;
+    }
+
+    private void sellItem(int id){
+        if(this.getItemCount(id)>0) {
+            totalRevenue += this.getItemPrice(id);
+            Items.get(getIdIndex(id)).count--;
+        }
+    }
+
+    private int getIdIndex(int id) {
+        for(int i=0; i<this.Items.size(); i++) {
+            if (this.Items.get(i).id == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    // return  0 => successfully added
+    // return -1 => rowsOverLoadException
+    // return -2 => columnsOverLoadException
+    // return -3 => idIsAlreadyExistException
+    public int addItem(int id, String name, int count, float price){
+        if(Items.size() >5){
+            return -1;
+        }else if(count > 5){
+            return -2;
+        }else if(this.getItemPrice(id)!= -1){
+            return -3;
+        }
+        else{
+            Items.add(new Item(id, name, count,price));
+            return 0;
+        }
+    }
+
+    // return -1 => ItemNotFoundException
+    // return n ; n>0 successfully returned item count
+    public int getItemCount(int id) {
+        for(int i=0; i<this.Items.size(); i++){
+            if(this.Items.get(i).id == id){
+                return this.Items.get(i).count;
+            }
+        }
+        return -1;
+    }
+
+    // return -1 => ItemNotFoundException
+    // return n ; n>0 successfully returned item count
+    public float getItemPrice(int id) {
+        for(int i=0; i<this.Items.size(); i++){
+            if(this.Items.get(i).id == id){
+                return this.Items.get(i).price;
+            }
+        }
+        return -1;
+    }
+    // return "-1" => ItemNotFoundException
+    // return string ; successfully returned item count
+    public String getItemName(int id) {
+        for(int i=0; i<this.Items.size(); i++){
+            if(this.Items.get(i).id == id){
+                return this.Items.get(i).name;
+            }
+        }
+        return "-1";
+    }
+
+    public ArrayList<String> getNames() {
+
+        ArrayList<String> names = new ArrayList<String>();
+         for (int i=0; i<this.Items.size();i++){
+             names.add(Items.get(i).name);
+         }
+
+        return names;
+    }
+
+    private ArrayList<String> getChange(float v)
+    {
+        ArrayList<String> change = new ArrayList<String>();
+        ArrayList<Double> coins = new ArrayList<Double>();
+
+        for (int i =0; i<currencies.size(); i++){
+            coins.add(Math.round( currencies.get(i).value* 100.0) / 100.0);
+        }
+        Collections.sort(coins,Collections.reverseOrder());
+
+
+        int i=0;
+        while(v>0 && i< coins.size()){
+            if(v>=coins.get(i)){
+                change.add(coins.get(i)+"");
+                v-=coins.get(i);
+            }else{
+                i++;
+            }
+        }
+        return change;
+    }
+
+    public void SelectItem(int id){
+        if(this.getItemCount(id)<=0){
+            System.out.println("sorry this Item Sold out");
+        }
+        Scanner in = new Scanner(System.in);
+        float price = getItemPrice(id);
+        float balance = 0;
+
+        int state=-1;
+        while (state!=0 ) {
+            System.out.println("Please select payment method:");
+            System.out.println("0. cancel order");
+            System.out.println("1. credit card");
+            System.out.println("2. coins and notes");
+
+
+            state = in.nextInt();
+            switch (state) {
+                case 1:
+                    System.out.println("please insert your Credit Card: :hint press \'y\' ");
+                    if(this.handleCreditCard(in.next())){
+                        this.sellItem(id);
+                        System.out.println("thanks for order from my SVM");
+                        state = 0;
+                    }else {
+                        System.out.println("Sorry, invalid credit card. please try again!");
+                        continue;
+                    }
+
+
+                    break;
+                case 2:
+                    System.out.println("please insert the money: ");
+                    //set timer to break the code after 30 sec
+                   try {
+                       while (balance < price) {
+                           float value = in.nextFloat();
+                           if(this.checkMoneyType(value)){
+                               balance+=value;
+                               System.out.println("current balance = "+balance);
+                           }else{
+                               System.out.println("Sorry Invalid currency!, insert another type \n or press \'x\' to cancel order");
+                               System.out.println("current balance = "+balance);
+                           }
+                       }
+                   }catch (InputMismatchException e){
+                       System.out.println("total change :" + balance);
+                       System.out.println("please take the change"+getChange(balance));
+                       System.out.println("your order has canceled!");
+                       System.out.println("thanks for order from my SVM");
+                       state = 0;
+                       break;
+                   }
+
+                    float change = (float) (Math.round((balance-price) * 100.0) / 100.0);
+                    System.out.println("total change :" + change);
+                    System.out.println("please take the change"+getChange(change));
+                    System.out.println("thanks for order from my SVM");
+                    state = 0;
+
+                    break;
+                default:
+                    System.out.println("sorry invalid input, pleas try again");
+            }
+
+        }
+
+
+    }
+
+    private boolean checkMoneyType(float v) {
+        for(int i=0; i< this.currencies.size(); i++){
+            if(this.currencies.get(i).value == v)
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean handleCreditCard(String s) {
+        if(s.equals("y")){
+            return true;
+        }
+        else return false;
+    }
+}
